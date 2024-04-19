d730cb373964e6698fa928b36207c3cd6a1eeec9
Nitukumari19 <129223835+Nitukumari19@users.noreply.github.com>
2024-04-19 20:11:25 +0530

    Add files via upload

---
+++ PRODIGY_WD2/script.js
@@ -0,0 +1,21 @@
+
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x= (window.innerwidth - e.pageX*speed)/120
+        const y= (window.innerwidth - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+
+    })
+}
+
+gsap.from('.logo',{opacity:0, duration:1, delay:2, y:10})
+gsap.from('.navbar .nav_item',{opacity:0, duration:1, delay:2.1, y:30, stagger:0.2})
+
+gsap.from('.title',{opacity:0,duration:1,delay:1.6,y:30})
+gsap.from('.description',{opacity:0,duration:1,delay:1.8,y:30})
+gsap.from('.btn',{opacity:0,duration:1,delay:2.1,y:30})
+gsap.from('.image',{opacity:0,duration:1,delay:2.6,y:30})
